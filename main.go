1d1addce189c5894adfb32f6216e56c1ccb373e0
Axel Siebenborn <axel.siebenborn@sap.com>
2018-07-24 15:33:29 +0200

    Check for PORT environment
    Co-authored-by: Ralf Pannemans <ralf.pannemans@sap.com>

--- main.go
+++ main.go
@@ -3,13 +3,27 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
-func helloWorld(w http.ResponseWriter, r *http.Request){
+const (
+	DEFAULT_PORT = "8080"
+)
+
+var (
+	port string
+)
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World")
 }
 
 func main() {
+
+	if port = os.Getenv("PORT"); len(port) == 0 {
+		port = DEFAULT_PORT
+	}
+
 	http.HandleFunc("/", helloWorld)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":"+port, nil)
 }
