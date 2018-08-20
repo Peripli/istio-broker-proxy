fce94e15cf57a6ba15dd0808c2e8012bc1fbcef5
Julia Plachetka <julia.plachetka@sap.com>
2018-08-20 11:48:52 +0200

    Add public function creating gateway, service entry and virtual service
    Co-authored-by: Ralf Pannemans <ralf.pannemans@sap.com>

---
+++ pkg/config/config.go
@@ -0,0 +1,94 @@
+package config
+
+import (
+	"github.com/ghodss/yaml"
+	"istio.io/api/networking/v1alpha3"
+	"istio.io/istio/pilot/pkg/config/kube/crd"
+	"istio.io/istio/pilot/pkg/model"
+)
+
+type generatedServiceEntry struct {
+	gateway        model.Config
+	virtualService model.Config
+	serviceEntry   model.Config
+}
+
+func CreateEntriesForExternalService(serviceName string, ipServiceEntry string, portServiceEntry uint32, hostVirtualService string, portVirtualService uint32) (string, error) {
+
+	var entry generatedServiceEntry
+
+	entry.gateway = createIngressGatewayForExternalService(hostVirtualService, 9000, serviceName, "client.istio.sapcloud.io")
+	entry.virtualService = createVirtualServiceForExternalService(hostVirtualService, portVirtualService, serviceName)
+	entry.serviceEntry = createServiceEntryForExternalService(ipServiceEntry, portServiceEntry, serviceName)
+
+	return toYamlArray(entry)
+}
+
+func createServiceEntryForExternalService(endpointAddress string, port uint32, serviceName string) model.Config {
+	hosts := []string{serviceName + ".service-fabrik"}
+	ports := v1alpha3.Port{Number: port, Name: "postgres", Protocol: "TCP"}
+	resolution := v1alpha3.ServiceEntry_STATIC
+	endpoint := v1alpha3.ServiceEntry_Endpoint{Address: endpointAddress}
+	serviceEntry := v1alpha3.ServiceEntry{Hosts: hosts, Ports: []*v1alpha3.Port{&ports}, Resolution: resolution,
+		Endpoints: []*v1alpha3.ServiceEntry_Endpoint{&endpoint}}
+	config := model.Config{Spec: &serviceEntry}
+	config.Name = serviceName + "-service-entry"
+
+	return config
+}
+
+func createVirtualServiceForExternalService(hostName string, port uint32, serviceName string) model.Config {
+	destination := v1alpha3.Destination{Host: serviceName + ".service-fabrik",
+		Port: &v1alpha3.PortSelector{Port: &v1alpha3.PortSelector_Number{Number: port}}}
+	route := v1alpha3.TCPRoute{Route: []*v1alpha3.DestinationWeight{&v1alpha3.DestinationWeight{Destination: &destination}}}
+	tcpRoutes := []*v1alpha3.TCPRoute{&route}
+	hosts := []string{hostName}
+	gateways := []string{serviceName + "-gateway"}
+	virtualService := v1alpha3.VirtualService{Tcp: tcpRoutes, Hosts: hosts, Gateways: gateways}
+	config := model.Config{Spec: &virtualService}
+	config.Name = serviceName + "-virtual-service"
+
+	return config
+}
+
+func createIngressGatewayForExternalService(hostName string, portNumber uint32, serviceName string, clientName string) model.Config {
+	port := v1alpha3.Port{Number: portNumber, Name: "tls", Protocol: "TLS"}
+	hosts := []string{hostName}
+	tls := v1alpha3.Server_TLSOptions{Mode: v1alpha3.Server_TLSOptions_MUTUAL,
+		ServerCertificate: "/var/vcap/jobs/envoy/config/certs/" + serviceName + ".crt",
+		PrivateKey:        "/var/vcap/jobs/envoy/config/certs/" + serviceName + ".key",
+		CaCertificates:    "/var/vcap/jobs/envoy/config/certs/ca.crt",
+		SubjectAltNames:   []string{clientName}}
+	gateway := v1alpha3.Gateway{Servers: []*v1alpha3.Server{&v1alpha3.Server{Port: &port, Hosts: hosts, Tls: &tls}}}
+	config := model.Config{Spec: &gateway}
+	config.Name = serviceName + "-gateway"
+
+	return config
+}
+
+func toText(schema model.ProtoSchema, config model.Config) (string, error) {
+	kubernetesConf, err := crd.ConvertConfig(schema, config)
+	if err != nil {
+		return "", err
+	}
+	bytes, err := yaml.Marshal(kubernetesConf)
+	return string(bytes), err
+}
+
+func toYamlArray(entry generatedServiceEntry) (string, error) {
+	kubernetesConf, err := crd.ConvertConfig(model.Gateway, entry.gateway)
+	if err != nil {
+		return "", err
+	}
+	var array []interface{}
+	array = append(array, kubernetesConf)
+
+	kubernetesConf, err = crd.ConvertConfig(model.ServiceEntry, entry.serviceEntry)
+	array = append(array, kubernetesConf)
+
+	kubernetesConf, err = crd.ConvertConfig(model.VirtualService, entry.virtualService)
+	array = append(array, kubernetesConf)
+
+	bytes, err := yaml.Marshal(array)
+	return string(bytes), err
+}
---
+++ pkg/config/config_test.go
@@ -0,0 +1,188 @@
+package config
+
+import (
+		. "github.com/onsi/gomega"
+			"istio.io/istio/pilot/pkg/model"
+	"testing"
+	"gopkg.in/yaml.v2"
+	"errors"
+)
+
+const (
+	gateway_yml = `apiVersion: networking.istio.io/v1alpha3
+kind: Gateway
+metadata:
+  creationTimestamp: null
+  name: postgres-server-gateway
+  namespace: default
+spec:
+  servers:
+  - hosts:
+    - postgres.services.cf.dev01.aws.istio.sapcloud.io
+    port:
+      name: tls
+      number: 9000
+      protocol: TLS
+    tls:
+      caCertificates: /var/vcap/jobs/envoy/config/certs/ca.crt
+      mode: MUTUAL
+      privateKey: /var/vcap/jobs/envoy/config/certs/postgres-server.key
+      serverCertificate: /var/vcap/jobs/envoy/config/certs/postgres-server.crt
+      subjectAltNames:
+      - client.istio.sapcloud.io
+`
+	virtual_service_yml = `apiVersion: networking.istio.io/v1alpha3
+kind: VirtualService
+metadata:
+  creationTimestamp: null
+  name: postgres-server-virtual-service
+  namespace: default
+spec:
+  gateways:
+  - postgres-server-gateway
+  hosts:
+  - postgres.services.cf.dev01.aws.istio.sapcloud.io
+  tcp:
+  - route:
+    - destination:
+        host: postgres-server.service-fabrik
+        port:
+          number: 47637
+`
+	service_entry_yml = `apiVersion: networking.istio.io/v1alpha3
+kind: ServiceEntry
+metadata:
+  creationTimestamp: null
+  name: postgres-server-service-entry
+  namespace: default
+spec:
+  endpoints:
+  - address: 10.11.241.0
+  hosts:
+  - postgres-server.service-fabrik
+  ports:
+  - name: postgres
+    number: 47637
+    protocol: TCP
+  resolution: STATIC
+`
+)
+
+
+
+func TestCompleteEntryNotEmpty(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	configAsString, _ := CreateEntriesForExternalService( "myservice", "10.10.10.10", 10,  "myservice.landscape", 20 )
+
+    var configObjects []interface{}
+	yaml.Unmarshal([]byte(configAsString), &configObjects)
+
+	g.Expect(configObjects).To(HaveLen(3))
+}
+
+func TestCompleteEntryGateway(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	configAsString, _ := CreateEntriesForExternalService( "myservice", "10.10.10.10", 10,  "myservice.landscape", 20 )
+
+	var configObjects []interface{}
+	yaml.Unmarshal([]byte(configAsString), &configObjects)
+
+	gatewayConfig, err := lookupObjectFromConfig(configObjects, "Gateway")
+	g.Expect(err).ShouldNot(HaveOccurred())
+
+	gatewaySpec, _ := yaml.Marshal(gatewayConfig["spec"])
+	gatewayMetadata, _ := yaml.Marshal(gatewayConfig["metadata"])
+
+	g.Expect(string(gatewaySpec)).To(ContainSubstring("myservice.landscape"))
+	g.Expect(string(gatewaySpec)).To(ContainSubstring("9000"))
+	g.Expect(string(gatewaySpec)).To(ContainSubstring("client.istio.sapcloud.io"))
+	g.Expect(string(gatewayMetadata)).To(ContainSubstring("name: myservice-gateway"))
+}
+
+func TestCompleteServiceEntry(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	configAsString, _ := CreateEntriesForExternalService( "myservice", "10.10.10.10", 156,  "myservice.landscape", 20 )
+
+	var configObjects []interface{}
+	yaml.Unmarshal([]byte(configAsString), &configObjects)
+
+	serviceEntryConfig, err := lookupObjectFromConfig(configObjects, "ServiceEntry")
+	g.Expect(err).ShouldNot(HaveOccurred())
+
+	serviceEntrySpec, _ := yaml.Marshal(serviceEntryConfig["spec"])
+	serviceEntryMetadata, _ := yaml.Marshal(serviceEntryConfig["metadata"])
+
+	g.Expect(string(serviceEntrySpec)).To(ContainSubstring("10.10.10.10"))
+	g.Expect(string(serviceEntrySpec)).To(ContainSubstring("156"))
+	//FIXME: check REAL SERVICE
+	//g.Expect(string(serviceEntrySpec)).To(ContainSubstring("name: myservice"))
+	g.Expect(string(serviceEntryMetadata)).To(ContainSubstring("name: myservice-service-entry"))
+}
+
+func TestCompleteVirtualService(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	configAsString, _ := CreateEntriesForExternalService( "myservice", "10.10.10.10", 156,  "myservice.landscape", 8765 )
+
+	var configObjects []interface{}
+	yaml.Unmarshal([]byte(configAsString), &configObjects)
+
+	virtualServiceConfig, err := lookupObjectFromConfig(configObjects, "VirtualService")
+	g.Expect(err).ShouldNot(HaveOccurred())
+
+	virtualServiceSpec, _ := yaml.Marshal(virtualServiceConfig["spec"])
+
+	g.Expect(string(virtualServiceSpec)).To(ContainSubstring("myservice.landscape"))
+	g.Expect(string(virtualServiceSpec)).To(ContainSubstring("8765"))
+	g.Expect(string(virtualServiceSpec)).To(ContainSubstring("host: myservice.service-fabrik"))
+}
+
+func lookupObjectFromConfig(configObjects []interface{}, kind string) (map[interface{}]interface{}, error) {
+
+	for _, entryUntyped := range configObjects {
+		entry := entryUntyped.(map[interface{}]interface{})
+		if entry["kind"] == kind {
+			return entry, nil
+		}
+	}
+
+	return nil, errors.New("not found:" + kind)
+}
+
+func TestGatewayFromGo(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	gateway := createIngressGatewayForExternalService("postgres.services.cf.dev01.aws.istio.sapcloud.io",
+		9000,
+		"postgres-server",
+		"client.istio.sapcloud.io")
+
+	text, err := toText(model.Gateway, gateway)
+	g.Expect(err).NotTo(HaveOccurred())
+	g.Expect(text).To(Equal(gateway_yml))
+}
+
+func TestVirtualServiceFromGo(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	virtualService := createVirtualServiceForExternalService("postgres.services.cf.dev01.aws.istio.sapcloud.io",
+		47637,
+		"postgres-server")
+
+	text, err := toText(model.VirtualService, virtualService)
+	g.Expect(err).NotTo(HaveOccurred())
+	g.Expect(text).To(Equal(virtual_service_yml))
+}
+
+func TestServiceEntryFromGo(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	virtualService := createServiceEntryForExternalService("10.11.241.0", 47637, "postgres-server")
+
+	text, err := toText(model.ServiceEntry, virtualService)
+	g.Expect(err).NotTo(HaveOccurred())
+	g.Expect(text).To(Equal(service_entry_yml))
+}
