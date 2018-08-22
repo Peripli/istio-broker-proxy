a9b7d209e820a526e65b0e520438d22c1f36d8aa
Julia Plachetka <julia.plachetka@sap.com>
2018-08-22 15:31:43 +0200

    Build one client configuration from parts
    Missing VirtualServices.
    
    Co-authored-by: Ralf Pannemans <ralf.pannemans@sap.com>

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -23,6 +23,31 @@ func CreateEntriesForExternalService(serviceName string, endpointServiceEntry st
 	return toYamlArray(entry)
 }
 
+func CreateEntriesForExternalServiceClient(serviceName string, hostName string, portNumber uint32) (string, error) {
+	var array []interface{}
+
+	gateway := createEgressGatewayForExternalService(hostName, 443, serviceName)
+	kubernetesConf, _ := crd.ConvertConfig(model.Gateway, gateway)
+	array = append(array, kubernetesConf)
+
+	destinationRule := createEgressDestinationRuleForExternalService(hostName, 9000, serviceName)
+	kubernetesConf, _ = crd.ConvertConfig(model.DestinationRule, destinationRule)
+	array = append(array, kubernetesConf)
+	array = append(array, kubernetesConf)
+	array = append(array, kubernetesConf)
+
+	serviceEntry := createEgressInternServiceEntryForExternalService(hostName, portNumber, serviceName)
+	kubernetesConf, _ = crd.ConvertConfig(model.ServiceEntry, serviceEntry)
+	array = append(array, kubernetesConf)
+
+	serviceEntry = createEgressExternServiceEntryForExternalService(hostName, 9000, serviceName)
+	kubernetesConf, _ = crd.ConvertConfig(model.ServiceEntry, serviceEntry)
+	array = append(array, kubernetesConf)
+
+	bytes, err := yaml.Marshal(array)
+	return string(bytes), err
+}
+
 func toYamlArray(entry generatedServiceConfig) (string, error) {
 	var array []interface{}
 
--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"github.com/ghodss/yaml"
 	. "github.com/onsi/gomega"
+	"regexp"
 	"testing"
 )
 
@@ -16,6 +17,17 @@ func TestCompleteEntryNotEmpty(t *testing.T) {
 	yaml.Unmarshal([]byte(configAsString), &configObjects)
 
 	g.Expect(configObjects).To(HaveLen(3))
+}
+
+func TestCompleteClientEntryNotEmpty(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	configAsString, _ := CreateEntriesForExternalServiceClient("myservice", "myservice.landscape", 1111)
+
+	var configObjects []interface{}
+	yaml.Unmarshal([]byte(configAsString), &configObjects)
+
+	g.Expect(configObjects).To(HaveLen(6))
 }
 
 func TestCompleteEntryGateway(t *testing.T) {
@@ -79,14 +91,94 @@ func TestCompleteVirtualService(t *testing.T) {
 	g.Expect(string(virtualServiceSpec)).To(ContainSubstring("host: myservice.service-fabrik"))
 }
 
+func TestCompleteEntryClientGateway(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	configAsString, _ := CreateEntriesForExternalServiceClient("myservice", "myservice.landscape", 12345)
+
+	var configObjects []interface{}
+	yaml.Unmarshal([]byte(configAsString), &configObjects)
+
+	gatewayConfig, err := lookupObjectFromConfig(configObjects, "Gateway")
+	g.Expect(err).ShouldNot(HaveOccurred())
+
+	gatewaySpec, _ := yaml.Marshal(gatewayConfig["spec"])
+	gatewayMetadata, _ := yaml.Marshal(gatewayConfig["metadata"])
+
+	g.Expect(string(gatewaySpec)).To(ContainSubstring("myservice.landscape"))
+	g.Expect(string(gatewaySpec)).To(ContainSubstring("443"))
+	g.Expect(string(gatewaySpec)).To(ContainSubstring("spiffe://cluster.local/ns/default/sa/default"))
+	g.Expect(string(gatewaySpec)).To(ContainSubstring("/etc/certs/cert-chain.pem"))
+
+	g.Expect(string(gatewayMetadata)).To(ContainSubstring("name: istio-egressgateway-myservice"))
+}
+
+func TestCompleteEntryClientDestinationRule(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	configAsString, _ := CreateEntriesForExternalServiceClient("myservice", "myservice.landscape", 12345)
+
+	var configObjects []interface{}
+	yaml.Unmarshal([]byte(configAsString), &configObjects)
+
+	ruleConfig, err := lookupObjectFromConfig(configObjects, "DestinationRule")
+	g.Expect(err).ShouldNot(HaveOccurred())
+
+	ruleSpec, _ := yaml.Marshal(ruleConfig["spec"])
+	ruleMetadata, _ := yaml.Marshal(ruleConfig["metadata"])
+
+	g.Expect(string(ruleSpec)).To(ContainSubstring("myservice.landscape"))
+	g.Expect(string(ruleSpec)).To(ContainSubstring("9000"))
+	g.Expect(string(ruleSpec)).To(ContainSubstring("sni: myservice.landscape"))
+
+	g.Expect(string(ruleMetadata)).To(ContainSubstring("egressgateway-myservice"))
+}
+
+func TestCompleteEntryClientServiceEntry(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	configAsString, _ := CreateEntriesForExternalServiceClient("myservice", "myservice.landscape", 12345)
+
+	var configObjects []interface{}
+	yaml.Unmarshal([]byte(configAsString), &configObjects)
+
+	serviceEntriesConfigs := lookupObjectsFromConfigs(configObjects, "ServiceEntry")
+	g.Expect(serviceEntriesConfigs).To(HaveLen(2))
+
+	var ports []string
+	for _, serviceEntryConfig := range serviceEntriesConfigs {
+		entrySpec, _ := yaml.Marshal(serviceEntryConfig)
+		g.Expect(string(entrySpec)).To(ContainSubstring("myservice.landscape"))
+
+		r := regexp.MustCompile(`number: (\d+)`)
+		port := r.FindStringSubmatch(string(entrySpec))[1]
+		ports = append(ports, port)
+	}
+	g.Expect(ports).To(ContainElement("12345"))
+	g.Expect(ports).To(ContainElement("9000"))
+}
+
 func lookupObjectFromConfig(configObjects []interface{}, kind string) (map[string]interface{}, error) {
+
+	array := lookupObjectsFromConfigs(configObjects, kind)
+
+	if array == nil {
+
+		return nil, errors.New("not found:" + kind)
+	} else {
+		return array[0], nil
+	}
+}
+
+func lookupObjectsFromConfigs(configObjects []interface{}, kind string) []map[string]interface{} {
+	var array []map[string]interface{}
 
 	for _, entryUntyped := range configObjects {
 		entry := entryUntyped.(map[string]interface{})
 		if entry["kind"] == kind {
-			return entry, nil
+			array = append(array, entry)
 		}
 	}
 
-	return nil, errors.New("not found:" + kind)
+	return array
 }
