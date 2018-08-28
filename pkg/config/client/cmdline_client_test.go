5e4c756e6a6061bbf8028b9b0b288d59649e08fc
Ralf Pannemans <ralf.pannemans@sap.com>
2018-08-28 17:31:03 +0200

    Add client test to get rid of disabled test warning

--- pkg/config/client/cmdline_client_test.go
+++ pkg/config/client/cmdline_client_test.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bytes"
-	"fmt"
 	"io"
 	"os"
 	"testing"
@@ -10,28 +9,28 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-func TestCmdClientWritesToStdout(t *testing.T) {
+func TestCmdClientClientCallDiffers(t *testing.T) {
 	g := NewGomegaWithT(t)
+	stdoutClient := callClient(true, "myservice", "myhost", 1234, "1.2.3.4")
+	stdoutServer := callClient(false, "myservice", "myhost", 1234, "1.2.3.4")
+
+	g.Expect(stdoutClient).NotTo(Equal(stdoutServer))
+}
+
+func callClient(clientConfig bool, serviceName string, hostVirtualService string, portServiceEntry int, endpointServiceEntry string) string {
 	oldStdout := os.Stdout
 	r, w, _ := os.Pipe()
 	os.Stdout = w
 
-	var args []string
-	args = append(args, "--help")
-	os.Args = args
-	main()
-
+	createOutput(clientConfig, serviceName, hostVirtualService, portServiceEntry, endpointServiceEntry)
 	outC := make(chan string)
 	go func() {
 		var buf bytes.Buffer
 		io.Copy(&buf, r)
 		outC <- buf.String()
 	}()
-
 	w.Close()
 	os.Stdout = oldStdout
 	stdout := <-outC
-
-	g.Expect(stdout).NotTo(BeEmpty())
-	fmt.Print(stdout)
+	return stdout
 }
