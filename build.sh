39754384ce7e39b8dd8edb060f4c283c4a3d4918
Ulrich Kramer <u.kramer@sap.com>
2018-09-05 13:50:20 +0200

    Removed additional GOPATH
    Co-authored-by: Holger Oehm <holger.oehm@sap.com>

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-export GOPATH=$GOPATH:${SCRIPT_DIR}/../../../..
+export GOPATH=${SCRIPT_DIR}/../../../..
 
 cd ${SCRIPT_DIR}
 
