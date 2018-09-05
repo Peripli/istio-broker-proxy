f20c30dc33031f34e017a1c8c39b49367d0c7754
Ulrich Kramer <u.kramer@sap.com>
2018-09-05 09:21:58 +0200

    Move build actions from pipeline to a script
    Co-authored-by: Holger Oehm <holger.oehm@sap.com>

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+export GOPATH=$GOPATH:$(pwd)/go
+
+go get -d -v github.infra.hana.ondemand.com/istio/istio-broker/...
+go build github.infra.hana.ondemand.com/istio/istio-broker/pkg/config/client
+./client --help
+go build -v github.infra.hana.ondemand.com/istio/istio-broker
+( cd go/src/github.infra.hana.ondemand.com/istio/istio-broker/test && ./test_update_credentials.sh )
