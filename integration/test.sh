ad87ba5710a439279bc89b54496b894536714ea0
Ulrich Kramer <u.kramer@sap.com>
2018-09-10 15:04:37 +0200

    Unset CGO_ENABLED to fix integration pipeline.
    Co-authored-by: Ralf Pannemans <ralf.pannemans@sap.com>

--- integration/test.sh
+++ integration/test.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-set -exuo pipefail
+set -euo pipefail
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 export GOPATH=${SCRIPT_DIR}/../../../../..
+export CGO_ENABLED=0
 
 cd ${SCRIPT_DIR}
 
