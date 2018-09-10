350f8e6b67f09af40c868e5dab78c0f53ed74cee
Ulrich Kramer <u.kramer@sap.com>
2018-09-10 14:43:11 +0200

    Added script to start tests
    Co-authored-by: Ralf Pannemans <ralf.pannemans@sap.com>

---
+++ integration/test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -euo pipefail
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+export GOPATH=${SCRIPT_DIR}/../../../../..
+
+cd ${SCRIPT_DIR}
+
+go get -d -v ../...
+go test --tags=integration -v
