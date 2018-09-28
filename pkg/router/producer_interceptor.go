28180efe2dacc6bc5ec5e7d666112de464f6395b
Ulrich Kramer <u.kramer@sap.com>
2018-09-28 18:01:44 +0200

    Restore "Add adaptCredentials to ServiceBrokerInterceptor""
    This reverts commit a173e4a3e138899420832541670b4c48d3decf91.
    
    Co-authored-by: Holger Oehm <holger.oehm@sap.com>

--- pkg/router/producer_interceptor.go
+++ pkg/router/producer_interceptor.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"github.infra.hana.ondemand.com/istio/istio-broker/pkg/config"
+	"github.infra.hana.ondemand.com/istio/istio-broker/pkg/credentials"
 	"github.infra.hana.ondemand.com/istio/istio-broker/pkg/model"
 	"github.infra.hana.ondemand.com/istio/istio-broker/pkg/profiles"
 	istioModel "istio.io/istio/pilot/pkg/model"
@@ -40,6 +41,10 @@ func (c ProducerInterceptor) postBind(request model.BindRequest, response model.
 		return nil, err
 	}
 	return &response, nil
+}
+
+func (c ProducerInterceptor) adaptCredentials(in []byte) ([]byte, error) {
+	return credentials.Update(in)
 }
 
 func (c ProducerInterceptor) writeIstioFilesForProvider(bindingId string, request *model.BindRequest, response *model.BindResponse) error {
