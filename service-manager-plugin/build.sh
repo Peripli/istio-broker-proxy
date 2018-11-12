3b6b9b3874026f7baeec2473c03f7455c9ae7d0e
Ulrich Kramer <u.kramer@sap.com>
2018-11-12 11:53:39 +0100

    Compile plugin with CGO_ENABLED=1 inside docker

--- service-manager-plugin/build.sh
+++ service-manager-plugin/build.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-set -euox pipefail
+set -euo pipefail
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-
-export GOPATH=${SCRIPT_DIR}/../../../../..
-
 cd ${SCRIPT_DIR}
 
-CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -v -a -buildmode=plugin  -o service-manager-plugin.so  ./main.go
 docker build .. -f ./Dockerfile -t gcr.io/sap-se-gcp-istio-dev/sb-istio-proxy-k8s
 docker push gcr.io/sap-se-gcp-istio-dev/sb-istio-proxy-k8s
 
