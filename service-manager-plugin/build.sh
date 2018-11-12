00676c5eb5a7e7302945de7cbb956653316474d4
Ulrich Kramer <u.kramer@sap.com>
2018-11-12 08:58:51 +0100

    Modify build script

--- service-manager-plugin/build.sh
+++ service-manager-plugin/build.sh
@@ -8,7 +8,7 @@ export GOPATH=${SCRIPT_DIR}/../../../../..
 
 cd ${SCRIPT_DIR}
 
-go build -v -a -installsuffix cgo -buildmode=plugin  -o service-manager-plugin.so  ./main.go
+go build -v -buildmode=plugin -o service-manager-plugin.so  ./main.go
 docker build .. -f ./Dockerfile -t gcr.io/sap-se-gcp-istio-dev/sb-istio-proxy-k8s
 docker push gcr.io/sap-se-gcp-istio-dev/sb-istio-proxy-k8s
 
