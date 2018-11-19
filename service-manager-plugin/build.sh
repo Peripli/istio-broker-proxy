f1e07a2d1774386bc69595321fa0ec937ee9e77d
Jakob Schmid <jakob.schmid@sap.com>
2018-11-19 16:45:15 +0100

    Use env variables HUB and TAG to distinguish images for different clusters.
    Co-authored-by: Axel Siebenborn <axel.siebenborn@sap.com>

--- service-manager-plugin/build.sh
+++ service-manager-plugin/build.sh
@@ -5,6 +5,11 @@ set -euo pipefail
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 cd ${SCRIPT_DIR}
 
-docker build .. -f ./Dockerfile -t gcr.io/sap-se-gcp-istio-dev/sb-istio-proxy-k8s
-docker push gcr.io/sap-se-gcp-istio-dev/sb-istio-proxy-k8s
+if [ -z $HUB ] || [ -z $TAG ]; then
+  echo "error: Environment variables HUB and TAG must be defined."
+  exit 1
+fi 
+
+docker build .. -f ./Dockerfile -t $HUB/sb-istio-proxy-k8s:$TAG
+docker push $HUB/sb-istio-proxy-k8s:$TAG
 
--- service-manager-plugin/install.sh
+++ service-manager-plugin/install.sh
@@ -4,18 +4,23 @@ set -euox pipefail
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
+if [ -z $HUB ] || [ -z $TAG ]; then
+  echo "error: Environment variables HUB and TAG must be defined."
+  exit 1
+fi
+
 export GOPATH=${SCRIPT_DIR}/../../../../..
 
 cd ${SCRIPT_DIR}
 cd $GOPATH/src/github.com/peripli/service-broker-proxy-k8s
 
-helm del --purge service-broker-proxy
+helm del --purge service-broker-proxy || true
 helm install \
     --name service-broker-proxy \
     --namespace service-broker-proxy \
     --set config.sm.url=https://service-manager-nocis.cfapps.dev01.aws.istio.sapcloud.io \
     --set sm.user=o1msSsCXOYA5WQ9MjGd+oYxD03CqhZrrsUQMt0IfTzI= \
     --set sm.password=wcRV6lrPNyb/apAXsSk2i1kxZ0fMfteMJ6GoI8VheT4= \
-    --set image.repository=gcr.io/sap-se-gcp-istio-dev/sb-istio-proxy-k8s \
-    --set image.tag=latest \
+    --set image.repository=$HUB/sb-istio-proxy-k8s \
+    --set image.tag=$TAG \
     charts/service-broker-proxy-k8s
