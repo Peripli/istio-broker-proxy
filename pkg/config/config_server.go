ef34f4c4d68dd130a86c30b61d97fa60c78f1993
Ulrich Kramer <u.kramer@sap.com>
2019-01-22 10:46:58 +0100

    Split createConfig methods to access objects directly without using model.Config
    Co-authored-by: Julia Plachetka <julia.plachetka@sap.com>

--- pkg/config/config_server.go
+++ pkg/config/config_server.go
@@ -1,62 +1,29 @@
 package config
 
 import (
-	"fmt"
-	"istio.io/api/networking/v1alpha3"
+	"github.com/gogo/protobuf/proto"
 	"istio.io/istio/pilot/pkg/model"
 )
 
-const ingressCertName = "cf-service"
-
 func createServiceEntryForExternalService(endpointAddress string, portNumber uint32, serviceName string) model.Config {
-	hosts := []string{createServiceHost(serviceName)}
-	portName := fmt.Sprintf("%s-%d", serviceName, portNumber)
 
-	ports := v1alpha3.Port{Number: portNumber, Name: portName, Protocol: "TCP"}
-	resolution := v1alpha3.ServiceEntry_STATIC
-	endpoint := v1alpha3.ServiceEntry_Endpoint{Address: endpointAddress}
-	serviceEntrySpec := v1alpha3.ServiceEntry{Hosts: hosts, Ports: []*v1alpha3.Port{&ports}, Resolution: resolution,
-		Endpoints: []*v1alpha3.ServiceEntry_Endpoint{&endpoint}}
-	config := model.Config{Spec: &serviceEntrySpec}
-	config.Type = serviceEntry
-	config.Name = serviceName + "-service-entry"
-
-	return config
-}
-
-func createServiceHost(serviceName string) string {
-	serviceHost := serviceName + ".service-fabrik"
-	return serviceHost
+	return wrapAsConfig(createRawServiceEntryForExternalService(endpointAddress, portNumber, serviceName),
+		serviceEntry, serviceName+"-service-entry")
 }
 
 func createIngressVirtualServiceForExternalService(hostName string, port uint32, serviceName string) model.Config {
-	destination := v1alpha3.Destination{Host: createServiceHost(serviceName),
-		Port: &v1alpha3.PortSelector{Port: &v1alpha3.PortSelector_Number{Number: port}}}
-	route := v1alpha3.TCPRoute{Route: []*v1alpha3.DestinationWeight{&v1alpha3.DestinationWeight{Destination: &destination}}}
-	tcpRoutes := []*v1alpha3.TCPRoute{&route}
-	hosts := []string{hostName}
-	gateways := []string{serviceName + "-gateway"}
-	virtualServiceSpec := v1alpha3.VirtualService{Tcp: tcpRoutes, Hosts: hosts, Gateways: gateways}
-	config := model.Config{Spec: &virtualServiceSpec}
-	config.Type = virtualService
-	config.Name = serviceName + "-virtual-service"
-
-	return config
+	return wrapAsConfig(createRawIngressVirtualServiceForExternalService(hostName, port, serviceName),
+		virtualService, serviceName+"-virtual-service")
 }
 
 func createIngressGatewayForExternalService(hostName string, portNumber uint32, serviceName string, clientName string) model.Config {
-	port := v1alpha3.Port{Number: portNumber, Name: "tls", Protocol: "TLS"}
-	hosts := []string{hostName}
-	certPath := "/var/vcap/jobs/envoy/config/certs/"
-	tls := v1alpha3.Server_TLSOptions{Mode: v1alpha3.Server_TLSOptions_MUTUAL,
-		ServerCertificate: certPath + ingressCertName + ".crt",
-		PrivateKey:        certPath + ingressCertName + ".key",
-		CaCertificates:    certPath + "ca.crt",
-		SubjectAltNames:   []string{clientName}}
-	gatewaySpec := v1alpha3.Gateway{Servers: []*v1alpha3.Server{&v1alpha3.Server{Port: &port, Hosts: hosts, Tls: &tls}}}
-	config := model.Config{Spec: &gatewaySpec}
-	config.Type = gateway
-	config.Name = serviceName + "-gateway"
+	return wrapAsConfig(createRawIngressGatewayForExternalService(hostName, portNumber, clientName),
+		gateway, serviceName+"-gateway")
+}
 
+func wrapAsConfig(spec proto.Message, typeName string, name string) model.Config {
+	config := model.Config{Spec: spec}
+	config.Type = typeName
+	config.Name = name
 	return config
 }
---
+++ pkg/config/config_server_raw.go
@@ -0,0 +1,46 @@
+package config
+
+import (
+	"fmt"
+	"istio.io/api/networking/v1alpha3"
+)
+
+const ingressCertName = "cf-service"
+
+func createRawServiceEntryForExternalService(endpointAddress string, portNumber uint32, serviceName string) *v1alpha3.ServiceEntry {
+	hosts := []string{createServiceHost(serviceName)}
+	portName := fmt.Sprintf("%s-%d", serviceName, portNumber)
+
+	ports := v1alpha3.Port{Number: portNumber, Name: portName, Protocol: "TCP"}
+	resolution := v1alpha3.ServiceEntry_STATIC
+	endpoint := v1alpha3.ServiceEntry_Endpoint{Address: endpointAddress}
+	return &v1alpha3.ServiceEntry{Hosts: hosts, Ports: []*v1alpha3.Port{&ports}, Resolution: resolution,
+		Endpoints: []*v1alpha3.ServiceEntry_Endpoint{&endpoint}}
+}
+
+func createServiceHost(serviceName string) string {
+	serviceHost := serviceName + ".service-fabrik"
+	return serviceHost
+}
+
+func createRawIngressVirtualServiceForExternalService(hostName string, port uint32, serviceName string) *v1alpha3.VirtualService {
+	destination := v1alpha3.Destination{Host: createServiceHost(serviceName),
+		Port: &v1alpha3.PortSelector{Port: &v1alpha3.PortSelector_Number{Number: port}}}
+	route := v1alpha3.TCPRoute{Route: []*v1alpha3.DestinationWeight{{Destination: &destination}}}
+	tcpRoutes := []*v1alpha3.TCPRoute{&route}
+	hosts := []string{hostName}
+	gateways := []string{serviceName + "-gateway"}
+	return &v1alpha3.VirtualService{Tcp: tcpRoutes, Hosts: hosts, Gateways: gateways}
+}
+
+func createRawIngressGatewayForExternalService(hostName string, portNumber uint32, clientName string) *v1alpha3.Gateway {
+	port := v1alpha3.Port{Number: portNumber, Name: "tls", Protocol: "TLS"}
+	hosts := []string{hostName}
+	certPath := "/var/vcap/jobs/envoy/config/certs/"
+	tls := v1alpha3.Server_TLSOptions{Mode: v1alpha3.Server_TLSOptions_MUTUAL,
+		ServerCertificate: certPath + ingressCertName + ".crt",
+		PrivateKey:        certPath + ingressCertName + ".key",
+		CaCertificates:    certPath + "ca.crt",
+		SubjectAltNames:   []string{clientName}}
+	return &v1alpha3.Gateway{Servers: []*v1alpha3.Server{{Port: &port, Hosts: hosts, Tls: &tls}}}
+}
