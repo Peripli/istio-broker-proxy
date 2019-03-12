c19f8fa20af6fc0aaaebb4b4cbca26b965175ac8
Axel Siebenborn <axel.siebenborn@sap.com>
2019-03-12 16:47:39 +0100

    set missing env variable in cleanup.sh
    Co-authored-by: Philipp Stehle <philipp.stehle@sap.com>

--- integration/cleanup.sh
+++ integration/cleanup.sh
@@ -6,6 +6,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 export GOPATH=${SCRIPT_DIR}/../../../../..
 export CGO_ENABLED=0
+export CLEANUP_ORPHANED_OBJECTS=true
 
 cd ${SCRIPT_DIR}
 
