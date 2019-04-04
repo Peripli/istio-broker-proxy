78701bb6c91d5c28abff86405983c2caafd5a63c
Julia Plachetka <julia.plachetka@sap.com>
2019-04-04 12:45:31 +0200

    Change CreateIsioObject in config store
    Co-authored-by: Ulrich Kramer <u.kramer@sap.com>

--- pkg/router/file_config_store.go
+++ pkg/router/file_config_store.go
@@ -1,50 +1,27 @@
 package router
 
-import (
-	"istio.io/istio/pilot/pkg/model"
-	"k8s.io/api/core/v1"
-)
-
-type fileConfigStore struct {
-	istioDirectory string
-}
-
-var _ ConfigStore = &fileConfigStore{}
-
-func (f *fileConfigStore) CreateIstioConfig(bindingID string, config []model.Config) error {
-	//var fileName string
-	//
-	//ymlPath := path.Join(f.istioDirectory, fileName) + ".yml"
-	//log.Printf("PATH to istio config: %v\n", ymlPath)
-	//file, err := os.Create(ymlPath)
-	//if nil != err {
-	//	return fmt.Errorf("Unable to write istio configuration to file '%s': %s", fileName, err.Error())
-	//}
-	//defer file.Close()
-	//
-	//fileContent, err := config.ToYamlDocuments([]model.Config{configuration})
-	//if nil != err {
-	//	return err
-	//}
-	//_, err = file.Write([]byte(fileContent))
-	//if nil != err {
-	//	return err
-	//}
-	return nil
-}
-
-func (f *fileConfigStore) DeleteIstioConfig(string, string) error {
-	panic("implement me")
-}
-
-func (f *fileConfigStore) CreateService(bindingID string, service *v1.Service) (*v1.Service, error) {
-	panic("implement me")
-}
-
-func (f *fileConfigStore) DeleteService(string) error {
-	panic("implement me")
-}
-
-func (f *fileConfigStore) getNamespace() string {
-	panic("implement me")
-}
+//type fileConfigStore struct {
+//	istioDirectory string
+//}
+//
+//var _ ConfigStore = &fileConfigStore{}
+//
+//func (f *fileConfigStore) CreateIstioConfig(bindingID string, config []model.Config) error {
+//	return nil
+//}
+//
+//func (f *fileConfigStore) DeleteIstioConfig(string, string) error {
+//	panic("implement me")
+//}
+//
+//func (f *fileConfigStore) CreateService(bindingID string, service *v1.Service) (*v1.Service, error) {
+//	panic("implement me")
+//}
+//
+//func (f *fileConfigStore) DeleteService(string) error {
+//	panic("implement me")
+//}
+//
+//func (f *fileConfigStore) getNamespace() string {
+//	panic("implement me")
+//}
--- pkg/router/file_config_store_test.go
+++ pkg/router/file_config_store_test.go
@@ -1,16 +1,10 @@
 package router
 
-import (
-	. "github.com/onsi/gomega"
-	"istio.io/istio/pilot/pkg/model"
-	"testing"
-)
-
-func TestFileConfigStore(t *testing.T) {
-	g := NewGomegaWithT(t)
-
-	fileCS := fileConfigStore{}
-	error := fileCS.CreateIstioConfig("binding-id", []model.Config{})
-
-	g.Expect(error).NotTo(HaveOccurred())
-}
+//func TestFileConfigStore(t *testing.T) {
+//	g := NewGomegaWithT(t)
+//
+//	fileCS := fileConfigStore{}
+//	error := fileCS.CreateIstioConfig("binding-id", []model.Config{})
+//
+//	g.Expect(error).NotTo(HaveOccurred())
+//}
