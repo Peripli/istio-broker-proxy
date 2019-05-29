5b52344156007bc33210d9cec4bf8563c9fc8d6a
Julia Plachetka <julia.plachetka@sap.com>
2019-05-29 15:14:30 +0200

    Add -X linker option to test.sh.

--- integration/test.sh
+++ integration/test.sh
@@ -12,4 +12,5 @@ export CGO_ENABLED=0
 cd ${SCRIPT_DIR}
 
 go get github.com/kubernetes-incubator/service-catalog/pkg/apis/servicecatalog/v1beta1
-go test -v -timeout 20m -run . --pgbench-output $PGBENCH_OUTPUT --pgbench-time 60
+BROKER_VERSION=`git rev-parse HEAD`
+go test -v -timeout 20m -run . --pgbench-output $PGBENCH_OUTPUT --pgbench-time 60 -ldflags="-X router.commitHash=$BROKER_VERSION" 
